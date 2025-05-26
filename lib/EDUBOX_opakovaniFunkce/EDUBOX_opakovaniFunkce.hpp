5513431b0f2f9c7250629591f9a40cdb3f30317e
Dalik0002 <daliborsliva20@gmail.com>
2025-05-26 22:33:06 +0200

    Refactor function declarations and improve comments in EDUBOX_opakovaniFunkce

--- lib/EDUBOX_opakovaniFunkce/EDUBOX_opakovaniFunkce.hpp
+++ lib/EDUBOX_opakovaniFunkce/EDUBOX_opakovaniFunkce.hpp
@@ -1,18 +1,27 @@
 #include <Arduino.h>
 
-// Cvičení 1: Funkce bez návratové hodnoty a bez parametru
-void sayHello();
 
-// Cvičení 2: Funkce bez návratové hodnoty s parametrem
+//Funkce bez návratové hodnoty a bez parametru
+void turnOnLED();
+
+
+
+//Funkce bez návratové hodnoty s parametrem
 void printNumber(int number);
 
-// Cvičení 3: Funkce bez návratové hodnoty s více parametry
+
+
+//Funkce bez návratové hodnoty s více parametry
 void printSum(int a, int b);
 
-// Cvičení 4: Funkce s návratovou hodnotou a bez parametru
+
+
+//Funkce s návratovou hodnotou a bez parametru
 int getNumber();
 
 
-// Cvičení 5: Funkce s návratovou hodnotou a více parametry
+
+
+//Funkce s návratovou hodnotou a více parametry
 int sumNumbers(int a, int b);
 
