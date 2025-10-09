5f389e4d54a3ade96444349b2731b63fa46baa84
SLI0123 <dalibor.sliva.st@vsb.cz>
2025-10-09 13:14:11 +0200

    small edit

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include <Arduino.h>
 
-#include <Light_Module.hpp>
+#include <EDUBOX_Light_Module.hpp>
 //#include <WiFi_Setup_Light_Module.hpp>
 
-#include <TempHum_Module.hpp>
+#include <EDUBOX_TempHum_Module.hpp>
 #include <WiFi_Setup_TempHum_Module.hpp>
 
 
